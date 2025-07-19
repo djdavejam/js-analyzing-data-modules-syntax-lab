bd8e3e7b915bb20e4ce99eb32a96adbdf2c4182d
David James <davidanthoneyjames@gmail.com>
2025-07-19 10:49:15 -0400

    This is the completed analyzing data modules syntax lab

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+function combineUsers(...args) {
+  const combinedObject = { users: [] };
+  args.forEach(arr => combinedObject.users.push(...arr));
+  combinedObject.merge_date = Date.today().toString("M/dd/yyyy");
+  return combinedObject;
+}
 
 
-
-module.exports = {
-  ...(typeof combineUsers !== 'undefined' && { combineUsers })
-};
+module.exports = { combineUsers };
